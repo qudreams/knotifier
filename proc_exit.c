c0672080801f3d0a7aa6e6ca43d2315bbfd3136b
屈梦 <qudreams123@163.com>
2019-02-25 22:57:34 +0800

    modify comment

--- proc_exit.c
+++ proc_exit.c
@@ -11,20 +11,12 @@
     #define PID(ts) ((ts)->tgid)
 #endif
 
-/*
- * 进程退出事件通知
- * 此处不要调用PID，因为PID获取的是进程的PID,
- * 而线程退出时也会调用进程事件通知(这个可能是因为Linux下线程是轻量级进程的原因)
- * 如果自保进程中的一个线程退出，而整个进程并未退出，
- * 此时内核功能模块是不需要关闭的，所以此处应该检查当前退出的线程ID,
- * 如果该线程ID是客户端的进程ID，则表示自保进程正在进行退出
- */
 static int kproc_exit(struct notifier_block *nb, unsigned long action,
                                         void *data)
 {
     struct task_struct *tsk = (struct task_struct *)data;
 
-    //当前线程与进程ID不同，则该退出通知不是进程退出通知，不执行下面动作
+    //is thread exiting?
     if(unlikely(tsk != tsk->group_leader))
 	   return 0;
 
